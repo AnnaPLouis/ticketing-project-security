44ac1c2e861b92e888b7cb1313b9b5b59e2d77d9
ozzy <ozzy@cydeo.com>
2022-11-15 12:28:28 -0500

    Security

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -67,7 +67,8 @@ public class SecurityConfig {
                 .logout()
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                     .logoutSuccessUrl("/login")
-                .and().build();
+                .and()
+                .build();
     }
 
 
